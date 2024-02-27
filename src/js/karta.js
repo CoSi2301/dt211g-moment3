3b7603f76bd7c6df45b1fc96ec563baf6805355a
Conny Sirviö <cosi2301@student.miun.se>
2024-02-27 02:08:39 +0100

    tester

--- src/js/karta.js
+++ src/js/karta.js
@@ -1,17 +1,3 @@
-let geocoder;
-let map;
-function initMapAndGeocode() {
-  geocoder = new google.maps.Geocoder();
-  let latlng = new google.maps.LatLng(-50.607037624196714, 165.97458631677614);
-  const mapID = "bc62d5e70866cb03";
-  let mapOptions = {
-    zoom: 13,
-    center: latlng,
-    mapId: mapID,
-  };
-  map = new google.maps.Map(document.getElementById("map-holder"), mapOptions);
-}
-
 function geocodeSearch() {
   let address = document.getElementById("search-input").value;
   const { AdvancedMarkerElement } = google.maps.importLibrary("marker");
