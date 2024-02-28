179fb83511e0431681a33ae8abdb4884a034d233
Conny Sirviö <cosi2301@student.miun.se>
2024-02-28 15:01:16 +0100

    snyggat till koden

--- src/js/karta.js
+++ src/js/karta.js
@@ -1,49 +1,65 @@
-var myMap = L.map("map-holder").setView(
-  [-50.607037624196714, 165.97458631677614],
-  13
-);
+document.addEventListener("DOMContentLoaded", function () {
+  // ------------------------------------------------------------------------------------------------
+  // KARTAN -----------------------------------------------------------------------------------------
+  // ----------------------------------------------------------------------------------------- KARTAN
 
-L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
-  maxZoom: 19,
-  attribution:
-    '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
-}).addTo(myMap);
+  let myMap = L.map("map-holder").setView(
+    [-50.607037624196714, 165.97458631677614],
+    13
+  );
 
-let geocoder = L.Control.Geocoder.nominatim();
-let showMarker;
+  L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+    maxZoom: 19,
+    attribution:
+      '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+  }).addTo(myMap);
 
-function mapSearch() {
-  let input = document.getElementById("search-input").value;
-  if (input.length > 0) {
-    geocoder.geocode(input, function (results) {
-      if (results.length > 0) {
-        let result = results[0];
+  // ------------------------------------------------------------------------------------------------
+  // SÖKFUNKTIONEN ----------------------------------------------------------------------------------
+  // ---------------------------------------------------------------------------------- SÖKFUNKTIONEN
 
-        if (showMarker) {
-          myMap.removeLayer(showMarker);
+  let geocoder = L.Control.Geocoder.nominatim();
+  let showMarker;
+
+  function mapSearch() {
+    let input = document.getElementById("search-input").value;
+    if (input.length > 0) {
+      geocoder.geocode(input, function (results) {
+        if (results.length > 0) {
+          let result = results[0];
+
+          if (showMarker) {
+            myMap.removeLayer(showMarker);
+          }
+          showMarker = L.marker([result.center.lat, result.center.lng])
+            .addTo(myMap)
+            .bounce()
+            .on("click", function () {
+              this.toggleBouncing();
+            });
+          myMap.setView([result.center.lat, result.center.lng], 13);
+        } else {
+          alert("Hittar ingen plats med det namnet, försök igen.");
         }
-        showMarker = L.marker([result.center.lat, result.center.lng])
-          .addTo(myMap)
-          .bounce()
-          .on("click", function () {
-            this.toggleBouncing();
-          });
-        myMap.setView([result.center.lat, result.center.lng], 13);
-      } else {
-        alert("Hittar ingen plats med det namnet, försök igen.");
-      }
-    });
+      });
+    }
   }
-}
 
-document.addEventListener("DOMContentLoaded", (event) => {
-  document.getElementById("search-button").addEventListener("click", mapSearch);
+  // ------------------------------------------------------------------------------------------------
+  // ENTER i SÖKRUTAN -------------------------------------------------------------------------------
+  // ------------------------------------------------------------------------------- ENTER i SÖKRUTAN
 
   document
     .getElementById("search-input")
     .addEventListener("keydown", function (event) {
-      if (event.key === "Enter" || KeyboardEvent.code === 13) {
+      if (event.key === "Enter") {
         mapSearch();
       }
     });
+
+  // ------------------------------------------------------------------------------------------------
+  // SÖKKNAPPEN -------------------------------------------------------------------------------------
+  // ------------------------------------------------------------------------------------- SÖKKNAPPEN
+
+  document.getElementById("search-button").addEventListener("click", mapSearch);
 });
